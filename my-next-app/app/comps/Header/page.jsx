0bd396003ebb953989b7fbae0d31df9d0053cf77
Lucas <lucasbitalia@gmail.com>
2025-02-14 10:41:59 +0100

    uitleg nextjs

---
+++ my-next-app/app/comps/Header/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function page() {
+    return (
+        <header className="grid">
+            <h1>NEXT.js</h1>
+            <nav>
+                <a href="">Showcase </a>
+                <a href="">Docs</a>
+                <a href="">Blog</a>
+                <a href="">Templates</a>
+                <a href="">Enterprise</a>
+            </nav>
+        </header>
+    )
+}
